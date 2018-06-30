6d338c219e233229f4df1face41a490811513e88
Tyokopai <take353@gmail.com>
2018-07-01 00:35:32 +0900

    add input form

--- App.js
+++ App.js
@@ -9,30 +9,29 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    newTodo: '',
+  }
+
+  onChangeText(newTodo) {
+    this.setState({ newTodo });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TextInput 
+          style={styles.form}
+          onChangeText={text => this.onChangeText(text)}
+        />
       </View>
     );
   }
@@ -41,18 +40,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 30,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  form: {
+    backgroundColor: '#eeeeee',
+    padding: 10,
   },
 });
